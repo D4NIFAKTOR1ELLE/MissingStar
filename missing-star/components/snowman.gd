3f839aa7a317454a5a44a61479efe174735a2497
DAAAAANIIIIIIIIII <73837003+D4NIFAKTOR1ELLE@users.noreply.github.com>
2025-11-29 14:37:14 +0100

    Frosty da Snowman

--- missing-star/components/snowman.gd
+++ missing-star/components/snowman.gd
@@ -5,14 +5,14 @@ extends CharacterBody2D
 @onready var viewport = $SubViewport
 @onready var sound = $AudioStreamPlayer
 @onready var camera = $Camera2D
-@onready var sprite = $Sprite
+@onready var sprite: AnimatedSprite2D = $Sprite
 @onready var animation = $AnimationPlayer
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -350.0
-const MAX_DASH_SPEED := 600.0
+const MAX_DASH_SPEED := 500.0
 
-var jump_range: int = 400
+var jump_range: int = 300
 var can_fly := true
 var is_dashing: bool = false
 var dash_velocity: Vector2
@@ -26,15 +26,19 @@ func initialise():
 
 func save_to():
 	viewport.world_2d = get_tree().root.get_world_2d()
-	viewport.size = Vector2i((get_node("../ColorRect").size.x), get_node("../ColorRect").size.y)
+	viewport.size = Vector2i(
+		(Globals.game.current_level.get_node("Independent/BG").size.x),
+		Globals.game.current_level.get_node("Independent/BG").size.y)
 	await RenderingServer.frame_post_draw
 	var vt = viewport.get_texture()
 	var img = vt.get_image()
+	print("Image saved!")
 	return img.save_png("user://Screenshot.png")
 
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		animation.play("idle")
+		set_process_input(true)
 		can_fly = true
 		jumps_left = MAX_JUMPS
 
@@ -43,7 +47,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		normal_movement(delta)
 
-	# Flip sprite
 	if velocity.x > 0:
 		sprite.flip_h = false
 	elif velocity.x < 0:
@@ -57,6 +60,8 @@ func dash_movement(delta: float):
 	dash_remaining_distance -= movement.length()
 
 	if dash_remaining_distance <= 0.0:
+		animation.play("dash_exit")
+		await animation.animation_finished
 		set_process_input(true)
 		is_dashing = false
 		velocity = Vector2.ZERO
@@ -74,11 +79,11 @@ func normal_movement(delta: float):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("left_click"):
-		fly()
+	if Input.is_action_pressed("left_click") and !is_dashing:
+		await fly()
 	if Input.is_action_just_pressed("right_click"):
 		save_to()
-	if Input.is_action_pressed("jump") and jumps_left > 0:
+	if Input.is_action_pressed("jump") and jumps_left > 0 and !is_dashing:
 		jump()
 		jumps_left -= 1
 
@@ -89,14 +94,21 @@ func jump():
 	sound.play()
 	animation.play("fall")
 
+func start_recovery():
+	animation.play("recovery")
+
 func fly():
 	if not can_fly:
 		return
 
+	animation.play("dash_init")
+	await animation.animation_finished
+
 	set_process_input(false)
 
-	var distance_to_star = (Globals.game.star.global_position - global_position).length()
-	var direction = (Globals.game.star.global_position - global_position).normalized()
+	var to_star = (Globals.game.star.global_position - global_position)
+	var distance_to_star = to_star.length()
+	var direction = to_star.normalized()
 
 	dash_remaining_distance = min(distance_to_star, jump_range)
 
--- missing-star/levels/level.gd
+++ missing-star/levels/level.gd
@@ -2,3 +2,7 @@ extends Node
 
 func _ready():
 	$LevelEnd/CollisionShape2D.set_deferred("disabled", false)
+
+	await get_tree().process_frame
+
+	Globals.game.snowman.sprite.animation = self.name
